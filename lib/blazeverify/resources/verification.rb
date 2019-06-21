9adfcc70c6627a110c1602607e4b1f4ad4234c84
Jarrett Lusso <jclusso@gmail.com>
2019-06-21 17:26:38 -0400

    Added support for mx_record and smtp_provider

--- lib/blazeverify/resources/verification.rb
+++ lib/blazeverify/resources/verification.rb
@@ -1,7 +1,8 @@
 module BlazeVerify
   class Verification < APIResource
     attr_accessor :accept_all, :did_you_mean, :disposable, :domain, :email,
-                  :free, :reason, :role, :score, :state, :tag, :user
+                  :free, :mx_record, :reason, :role, :score, :smtp_provider,
+                  :state, :tag, :user
 
     %w(accept_all disposable free role).each do |method|
       define_method("#{method}?") do
