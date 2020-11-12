fbce2e9050574c8a0bc63672fbbabb1edb33db50
Jarrett Lusso <jclusso@gmail.com>
2020-11-12 10:58:54 -0500

    Added options validation to validator and additional test coverage.

--- lib/blazeverify/email_validator.rb
+++ lib/blazeverify/email_validator.rb
@@ -13,17 +13,28 @@
 class EmailValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
+    smtp = boolean_option_or_raise_error(:smtp, true)
+
+    states = options.fetch(:states, %i(deliverable risky unknown))
+    allowed_states = %i[deliverable undeliverable risky unknown]
+    unless (states - allowed_states).empty?
+      raise ArgumentError, ":states must be an array of symbols containing "\
+        "any or all of :#{allowed_states.join(', :')}"
+    end
+
+    free = boolean_option_or_raise_error(:free, true)
+    role = boolean_option_or_raise_error(:role, true)
+    disposable = boolean_option_or_raise_error(:disposable, false)
+    accept_all = boolean_option_or_raise_error(:accept_all, false)
+
+    timeout = options.fetch(:timeout, 3)
+    unless timeout.is_a?(Integer) && timeout > 1
+      raise ArgumentError, ":timeout must be an Integer greater than 1"
+    end
+
     return if record.errors[attribute].present?
     return unless value.present?
     return unless record.changes[attribute].present?
-
-    smtp = options.fetch(:smtp, true)
-    states = options.fetch(:states, %i(deliverable risky unknown))
-    free = options.fetch(:free, true)
-    role = options.fetch(:role, true)
-    disposable = options.fetch(:disposable, false)
-    accept_all = options.fetch(:accept_all, false)
-    timeout = options.fetch(:timeout, 3)
 
     ev = BlazeVerify.verify(value, timeout: timeout, smtp: smtp)
 
@@ -31,6 +42,9 @@ class EmailValidator < ActiveModel::EachValidator
     if record.respond_to?(result_accessor)
       record.instance_variable_set("@#{result_accessor}", ev)
     end
+
+    # if response is taking too long
+    return unless ev.respond_to?(:state)
 
     error ||= ev.state unless states.include?(ev.state.to_sym)
     error ||= :free if ev.free? && !free
@@ -42,6 +56,17 @@ class EmailValidator < ActiveModel::EachValidator
     record.errors.add(attribute, translation) if error
   rescue BlazeVerify::Error
     # silence errors
+  end
+
+  private
+
+  def boolean_option_or_raise_error(name, default)
+    option = options.fetch(name, default)
+    unless [true, false].include?(option)
+      raise ArgumentError, ":#{name} must by a Boolean"
+    end
+
+    option
   end
 
 end
--- test/email_validator_test.rb
+++ test/email_validator_test.rb
@@ -2,15 +2,23 @@ require 'test_helper'
 
 class EmailValidatorTest < Minitest::Test
 
-  def user_class
+  def user_class(
+    smtp: true, states: %i[deliverable risky unknown], free: true, role: true,
+    accept_all: true, disposable: true, timeout: 3
+  )
     Class.new do
       include ActiveModel::Model
       attr_accessor :email, :email_verification_result
 
       validates :email, presence: true, email: {
-        smtp: true, states: %i[deliverable risky unknown],
-        free: true, role: true, disposable: true, accept_all: true
+        smtp: smtp, states: states,
+        free: free, role: role, disposable: disposable, accept_all: accept_all,
+        timeout: timeout
       }
+
+      def self.name
+        'TestClass'
+      end
 
       # stub changes to always be true
       def changes
@@ -44,6 +52,34 @@ class EmailValidatorTest < Minitest::Test
 
     refute_nil @user.email_verification_result
     assert @user.email_verification_result.state, :undeliverable
+  end
+
+  def test_boolean_options
+    %i[smtp free role disposable accept_all].each do |option|
+      invalid_user = user_class(option => 'string').new
+      valid_user = user_class.new
+
+      assert !valid_user.valid?
+      assert_raises(ArgumentError) { invalid_user.valid? }
+    end
+  end
+
+  def test_states_option
+    invalid_user = user_class(states: %i[invalid_state]).new
+    valid_user = user_class.new
+
+    assert !valid_user.valid?
+    assert_raises(ArgumentError) { invalid_user.valid? }
+  end
+
+  def test_timeout_option
+    invalid_user1 = user_class(timeout: 'string').new
+    invalid_user2 = user_class(timeout: 1).new
+    valid_user = user_class.new
+
+    assert !valid_user.valid?
+    assert_raises(ArgumentError) { invalid_user1.valid? }
+    assert_raises(ArgumentError) { invalid_user2.valid? }
   end
 
 end
