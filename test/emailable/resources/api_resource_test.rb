3013238f465df29874b811814e9e535cb4c98185
Andrew Burleson <andrew@cosell.io>
2021-11-23 12:18:52 -0600

    Add #to_json on APIResources
    This PR adds a to_h and to_json method on API resources. It's helpful if you want to get back the raw data that an Emailable API Request returned, for example to serialize and cache that data for future use.

---
+++ test/emailable/resources/api_resource_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+module Emailable
+  class BatchTest < Minitest::Test
+    class Example < APIResource
+      attr_accessor :foo, :bar, :baz
+    end
+
+    def setup
+      @e = Example.new(foo: 'abc', bar: 123, baz: true)
+    end
+
+    def test_init
+      assert_equal 'abc', @e.foo
+      assert_equal 123,   @e.bar
+      assert_equal true,  @e.baz
+    end
+
+    def test_to_h
+      correct = {
+        foo: 'abc',
+        bar: 123,
+        baz: true,
+      }
+      assert_equal correct, @e.to_h
+      assert_equal correct, @e.to_hash
+    end
+
+    def test_to_json
+      correct = %q|{"foo":"abc","bar":123,"baz":true}|
+      assert_equal correct, @e.to_json
+    end
+
+    def test_inspect
+      correct = <<~STR.chomp
+        #<> JSON: {
+          "foo": "abc",
+          "bar": 123,
+          "baz": true
+        }
+      STR
+      output = @e.inspect.gsub(/<.*>/, '<>')
+      assert_equal correct, output
+    end
+  end
+end
