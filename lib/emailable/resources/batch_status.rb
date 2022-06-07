d49b6d8de7821b818572a9485d80c19b6be5b98a
evan berquist <e@cache.vc>
2022-06-07 16:34:50 -0400

    added download_file accessor for batch status. bump version

--- lib/emailable/resources/batch_status.rb
+++ lib/emailable/resources/batch_status.rb
@@ -1,7 +1,7 @@
 module Emailable
   class BatchStatus < APIResource
     attr_accessor :emails, :id, :message, :reason_counts, :total_counts,
-      :processed, :total
+      :processed, :total, :download_file
 
     def complete?
       message.include?('completed')
